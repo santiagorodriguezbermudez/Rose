8818996b8f54a2873bf979238acb3c1abc4cbad9
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-05-01 17:54:46 -0500

    Converted Header component to a Flexbox using Material Design Grid

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
 
 function Header() {
 
@@ -18,9 +19,6 @@ function Header() {
             margin: "0 auto",
             height: "40rem",
             maxWidth: "1440px",
-            display: "flex",
-            alignItems: "center",
-            padding: "5rem",
         },
         txtContainer: {
             display: "flex",
@@ -45,14 +43,19 @@ function Header() {
 
 
     return(
-        <div className={classes.imgContainer} id="home">
-            <div className={classes.txtContainer}>
+        <Grid container className={classes.imgContainer} justify="space-around" direction="row" alignItems="center" wrap="nowrap">
+            
+            <Grid item className={classes.txtContainer}>
                 <h1>Hire globally; Rose will comply locally so you can sleep peacefully at night</h1>
                 <h3>Rose combines legal structure with technology to support your global workforce. <strong>All in the cloud.</strong></h3>
                 <Button variant="contained" className={classes.button} color="primary" size="large">Schedule a Demo</Button>
-            </div>
-            <img className={classes.countryImg} src="img/Countries.svg" alt="countries-img"/>
-        </div>
+            </Grid>
+
+            <Grid item>
+                <img className={classes.countryImg} src="img/Countries.svg" alt="countries-img"/>
+            </Grid>
+            
+        </Grid>
     )
 }
 
