506dcf6368a6b70ebe68cc596e4f5eba262ada2a
SantiagoRodriguezBermudez <5817056+santiagorodriguezbermudez@users.noreply.github.com>
2020-05-02 16:15:57 -0500

    Styled Flexbox for mobile version

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,35 +3,38 @@ import { makeStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 import Grid from '@material-ui/core/Grid';
 
+
 function Header() {
 
     const useStyles = makeStyles((theme) => ({
         root: {
             flexGrow: 1,
-        },
-        img: {
-            width: "100%",
-        },
-        imgContainer: {
             backgroundImage: "url(\"img/Hero.png\")",
             backgroundSize: "cover",
             backgroundRepeat: "no-repeat",
             margin: "0 auto",
-            height: "40rem",
+            minHeight: "40rem",
             maxWidth: "1440px",
+            padding: "0 2rem",
+        },
+        img: {
+            width: "100%",
         },
         txtContainer: {
-            display: "flex",
-            flexDirection: "column",
             textAlign: "left",
             color: "white",
-            marginRight: "12rem",
         }, 
         button: {
-            marginTop: "3rem",
+            marginTop: "1rem",
         },
         countryImg: {
-            height: "99%",
+            maxWidth: "30rem",
+            height: "auto",
+            minHeight: "5rem",
+            margin: "2rem",
+        }, 
+        container: {
+            paddingTop: "4rem",
         }
 
       }));
@@ -43,19 +46,23 @@ function Header() {
 
 
     return(
-        <Grid container className={classes.imgContainer} justify="space-around" direction="row" alignItems="center" wrap="nowrap">
-            
-            <Grid item className={classes.txtContainer}>
-                <h1>Hire globally; Rose will comply locally so you can sleep peacefully at night</h1>
-                <h3>Rose combines legal structure with technology to support your global workforce. <strong>All in the cloud.</strong></h3>
-                <Button variant="contained" className={classes.button} color="primary" size="large">Schedule a Demo</Button>
-            </Grid>
+        <div className={classes.root}>
+            <Grid className={classes.container}container justify="space-around" alignItems="center" >
+                
+                <Grid item className={classes.txtContainer} xs={12} sm={12} md={6} container justify="center" direction="column" >
+                    
+                    <Grid item sm={12}><h1>Hire globally; Rose will comply locally so you can sleep peacefully at night</h1></Grid>
+                    <Grid item sm={12}><h3>Rose combines legal structure with technology to support your global workforce. <strong>All in the cloud.</strong></h3></Grid>
+                    <Grid item sm={12}><Button variant="contained" className={classes.button} color="primary" size="large">Schedule a Demo</Button></Grid>
+                    
+                </Grid>
 
-            <Grid item>
-                <img className={classes.countryImg} src="img/Countries.svg" alt="countries-img"/>
+                <Grid item xs={12} sm={12}  className={classes.countryImg} >
+                    <img src="img/Countries.svg" alt="countries-img"/>
+                </Grid>
+                
             </Grid>
-            
-        </Grid>
+        </div>
     )
 }
 
